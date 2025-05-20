8208e41ef2333cfbb443522a0ce56e07ef35ef8d
Mohammad Adil Rasheed <79353602+ErAdilrasheed@users.noreply.github.com>
2025-05-20 17:09:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
-  // write your code here
+	const pass = "";
+	const str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	for(let i =1; i<l; i++){
+		let char = Math.floor(Math.random() * str.l + 1);
+		pass+=str.charAt(char)
+	}
+	return pass;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
